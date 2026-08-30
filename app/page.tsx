868ea97815740ee16947569ab2cad926782c2a0f
alilops21-prog <alilops21@gmail.com>
2026-08-30 01:22:14 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>ROUSEATRA</h1>
+
+      <p>
+        Sea • Restaurant • Experience
+      </p>
+
+      <h2>Vivez l’expérience en mer</h2>
+
+      <p>
+        Réservez votre table à bord et découvrez
+        notre expérience gastronomique en Méditerranée.
+      </p>
+
+      <button>
+        Réserver ma table
+      </button>
+    </main>
+  );
+}
